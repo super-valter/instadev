ea5e5e3b651bb66470fa4ccf82c59f8bb49d7bc1
super-valter <valter@valtinho.com.br>
2025-01-13 19:22:16 -0300

    menu superior

--- src/components/TopBar/index.vue
+++ src/components/TopBar/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    top
+  <q-page class="row items-center justify-between q-mx-xl">
+    <q-img src="../../assets/camera.svg" width="24px" />
+    <q-img src="../../assets/Instagram_logo.svg" width="105px" />
+    <div>
+      <q-img src="../../assets/reels.svg" width="24px" class="q-mr-md" />
+      <q-img src="../../assets/direct.svg" width="24px" />
+    </div>
+    <div class="q-mt-md">
+      <q-toggle
+        :model-value="$q.dark.isActive"
+        :checked-icon="symRoundedDarkMode"
+        :unchecked-icon="symRoundedLightMode"
+        size="xl"
+        @update:model-value="$q.dark.toggle()"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -8,4 +22,10 @@
 defineOptions({
   name: "TopBar",
 });
+
+import {
+  symRoundedDarkMode,
+  symRoundedLightMode,
+} from '@quasar/extras/material-symbols-rounded';
+
 </script>
--- src/pages/Main/index.vue
+++ src/pages/Main/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page>
-    <top-bar />
+  <q-page class="flex flex-center">
+    <div class="container full-width ">
+      <top-bar />
+    </div>
   </q-page>
 </template>
 <script setup lang="ts">
@@ -8,5 +10,11 @@ defineOptions({
   name: "MainPage",
 });
 
-import TopBar from "src/components/TopBar/index.vue";
+import topBar from "components/TopBar/index.vue";
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 720px;
+}
+</style>
